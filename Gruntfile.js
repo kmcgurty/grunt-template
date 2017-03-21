ab28de74d4e1d9b9dded3360182f26e8c8a4d449
kmcgurty1 <kmcgurty1@gmail.com>
2017-03-21 11:53:20 -0400

    no longer minify js

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
 
             scripts: {
                 files: ['src/javascript/*.js'],
-                tasks: ['buildjs']
+                tasks: ['copy']
             },
 
             css: {
@@ -28,27 +28,10 @@ module.exports = function(grunt) {
         },
 
         jshint: {
-            all: [  '*.js', 'src/javascript/*.js'],
+            all: ['*.js', 'src/javascript/*.js'],
             options: {
                 "esnext": true,
                 "asi": true
-            }
-        },
-
-
-        uglify: {
-            main: {
-                options: {
-                    sourceMap: false,
-                    preserveComments: 'some',
-                    banner: '/* \n' +
-                        ' Source: https://github.com/kmcgurty/ \n' +
-                        ' Build date: <%= grunt.template.today("mm-dd-yyyy") %> \n' +
-                        '*/ \n "use strict" \n',
-                },
-                files: {
-                    'build/javascript/main.min.js': ['src/javascript/lib/*.js', 'src/javascript/*.js']
-                }
             }
         },
 
@@ -77,7 +60,7 @@ module.exports = function(grunt) {
                 logFn: function(req, res, error) {
                     if (res.statusCode === 200) {
                         console.log(req.method.cyan, '(' + res.statusCode + ')', req.url);
-                    } else {
+                    } else if (res.statusCode != "/favicon") {
                         console.log(req.method.red, '(' + res.statusCode + ')', req.url);
                     }
                 }
@@ -99,8 +82,13 @@ module.exports = function(grunt) {
                 }, {
                     expand: true,
                     flatten: true,
-                    src: 'src/javascript/libs/*.js',
-                    dest: 'build/javascript/libs/'
+                    src: 'src/javascript/*.js',
+                    dest: 'build/javascript/'
+                }, {
+                    expand: true,
+                    flatten: true,
+                    src: 'src/javascript/lib/*.js',
+                    dest: 'build/javascript/lib/'
                 }, {
                     expand: true,
                     flatten: true,
@@ -113,14 +101,12 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-http-server');
-    
+
     grunt.registerTask('default', ['buildAll', 'http-server', 'watch']);
 
-    grunt.registerTask('buildjs', ['jshint', 'uglify']);
     grunt.registerTask('buildcss', ['sass']);
-    grunt.registerTask('buildAll', ['buildjs', 'buildcss', 'copy']);
+    grunt.registerTask('buildAll', ['buildcss', 'copy']);
 };
